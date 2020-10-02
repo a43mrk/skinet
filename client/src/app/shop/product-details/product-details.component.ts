769f93a5c1f4b66d4cb5689128e8124c1ae375e9
Adem Sasaki <ricardokahurosasaki@halia.com.tr>
2020-10-02 10:46:53 +0300

    ch.127

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { ShopService } from './../shop.service';
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/products';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -20,7 +21,12 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     this.shopService.getProduct(
       +this.activateRoute.snapshot.paramMap.get('id')
-    ).subscribe(response => this.product = response, error => console.log(error));
+    ).subscribe(
+      response =>{
+        this.product = response;
+        this.bcService.set('@productDetails', response.name);
+      },
+      error => console.log(error));
   }
 
 }
