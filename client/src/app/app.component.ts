43d768d43c0f44d6d512828041d7aec315787b1a
Adem Sasaki <ricardokahurosasaki@halia.com.tr>
2020-10-05 09:54:55 +0300

    ch.150

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from './basket/basket.service';
 import { IPagination } from './shared/models/pagination';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -12,7 +13,17 @@ import { IProduct } from './shared/models/products';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor( ){ }
+  constructor(private basketService: BasketService ){ }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialized basket');
+      },
+      error => {
+        console.log(error);
+      });
+    }
+  }
 }
