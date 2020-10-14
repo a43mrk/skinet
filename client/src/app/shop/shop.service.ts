58b953b90259f725df528db7410f0148f6d3a3d8
Adem Sasaki <ricardokahurosasaki@halia.com.tr>
2020-10-14 13:44:55 +0300

    ch.283

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,12 +6,17 @@ import { IBrand } from '../shared/models/brands';
 import { IType } from '../shared/models/productType';
 import { map, delay } from 'rxjs/operators';
 import { IProduct } from '../shared/models/products';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
+  // 283-1 caching
+  products: IProduct[] = [];
+  brands: IBrand[] = [];
+  types: IType[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -36,19 +41,51 @@ export class ShopService {
     return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params })
       .pipe(
         // delay(1000),
-        map(response => { return response.body; })
+        map(response => {
+          // 183-2 save to cache
+          this.products = response.body.data;
+          return response.body;
+        })
       );
   }
 
   getBrands(){
-    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+    // 183-5 caching for brands
+    if (this.brands.length > 0){
+      return of(this.brands);
+    }
+
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands').pipe(
+      map(response => {
+        // 183-7 populate brands cache when retrieving from backend.
+        this.brands = response;
+        return response;
+      })
+    );
   }
 
   getTypes() {
-    return this.http.get<IType[]>(this.baseUrl + 'products/types');
+    // 183-4 caching for types
+    if (this.types.length > 0) {
+      return of(this.types);
+    }
+
+    return this.http.get<IType[]>(this.baseUrl + 'products/types').pipe(
+      map(response => {
+        // 183-6 populate the cache when retrieving from backend
+        this.types = response;
+        return response;
+      })
+    );
   }
 
   getProduct(id: number){
+    // 183-3 caching for getProduct
+    const product = this.products.find(p => p.id === id);
+    if (product) {
+      // return observable of product
+      return of(product);
+    }
     return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 }
